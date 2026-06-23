221a8cf866e407cf4e80e27d8b142db44b838eb1
rekha <132309074+rekhamarcus@users.noreply.github.com>
2026-06-23 15:11:16 -0700

    added figures; updated AHP data wrangling script

--- Scripts/AHP_data_wrangling.R
+++ Scripts/AHP_data_wrangling.R
@@ -28,11 +28,19 @@ maintaining.clim <- ssp5 %>% group_by(EST_ID) %>% summarize(mean.pr = min(mean.p
                                                             ES0.99.pr = min(ES0.99.pr),
                                                             mean.tas = min(mean.tas),
                                                             SD.tas = min(SD.tas),
-                                                            ES0.99 = min(ES0.99.tas))
+                                                            ES0.99.tas = min(ES0.99.tas))
+
+#take absolute value of climate variables to evaluate estuaries on total change, not direction
+maintaining.clim$mean.pr.abs <- abs(maintaining.clim$mean.pr)
+maintaining.clim$SD.pr.abs <- abs(maintaining.clim$SD.pr)
+maintaining.clim$ES0.99.pr.abs <- abs(maintaining.clim$ES0.99.pr)
+maintaining.clim$mean.tas.abs <- abs(maintaining.clim$mean.tas)
+maintaining.clim$SD.tas.abs <- abs(maintaining.clim$SD.tas)
+maintaining.clim$ES0.99.tas.abs <- abs(maintaining.clim$ES0.99.tas)
 
 maintaining <- full_join(maintaining.clim, birds)
 maintaining <- full_join(maintaining, slr)
-maintaining <- maintaining[,c(1:9,20,21)]
+maintaining <- maintaining[,c(1:15,26,27)]
 
 # 2 - improving - climate max SSP 1, all birds, max SSP 1
 
@@ -41,11 +49,19 @@ improving.clim <- ssp1 %>% group_by(EST_ID) %>% summarize(mean.pr = max(mean.pr)
                                                             ES0.99.pr = max(ES0.99.pr),
                                                             mean.tas = max(mean.tas),
                                                             SD.tas = max(SD.tas),
-                                                            ES0.99 = max(ES0.99.tas))
+                                                            ES0.99.tas = max(ES0.99.tas))
+
+#take absolute value of climate variables to evaluate estuaries on total change, not direction
+improving.clim$mean.pr.abs <- abs(improving.clim$mean.pr)
+improving.clim$SD.pr.abs <- abs(improving.clim$SD.pr)
+improving.clim$ES0.99.pr.abs <- abs(improving.clim$ES0.99.pr)
+improving.clim$mean.tas.abs <- abs(improving.clim$mean.tas)
+improving.clim$SD.tas.abs <- abs(improving.clim$SD.tas)
+improving.clim$ES0.99.tas.abs <- abs(improving.clim$ES0.99.tas)
 
 improving <- full_join(improving.clim, birds)
 improving <- full_join(improving, slr)
-improving <- improving[,c(1:9,19,21)]
+improving <- improving[,c(1:15,25,27)]
 
 # 3 - shorebirds - climate median, shorebirds, slr median
 
@@ -54,24 +70,33 @@ median.clim <- climate %>% group_by(EST_ID) %>% summarize(mean.pr = median(mean.
                                                           ES0.99.pr = median(ES0.99.pr),
                                                           mean.tas = median(mean.tas),
                                                           SD.tas = median(SD.tas),
-                                                          ES0.99 = median(ES0.99.tas))
+                                                          ES0.99.tas = median(ES0.99.tas))
+
+#take absolute value of climate variables to evaluate estuaries on total change, not direction
+median.clim$mean.pr.abs <- abs(median.clim$mean.pr)
+median.clim$SD.pr.abs <- abs(median.clim$SD.pr)
+median.clim$ES0.99.pr.abs <- abs(median.clim$ES0.99.pr)
+median.clim$mean.tas.abs <- abs(median.clim$mean.tas)
+median.clim$SD.tas.abs <- abs(median.clim$SD.tas)
+median.clim$ES0.99.tas.abs <- abs(median.clim$ES0.99.tas)
+
 slr$meanrisk <- (slr$risk1 + slr$risk5)/2
 
 shorebirds <- full_join(median.clim, birds)
 shorebirds <- full_join(shorebirds, slr)
-shorebirds <- shorebirds[,c(1:7,10:11,23,21)]
+shorebirds <- shorebirds[,c(1:13,16:17,29,27)]
 
 # 4 - waterfowl - climate median, waterfow, slr median
 
 waterfowl <- full_join(median.clim, birds)
 waterfowl <- full_join(waterfowl, slr)
-waterfowl <- waterfowl[,c(1:7,12:13,23,21)]
+waterfowl <- waterfowl[,c(1:13,18:19,29,27)]
 
 # normalize all data ---------------------------------------
 
 #range normalizing to ensure all values fall between 0 and 1
 
-for(i in 2:11){
+for(i in 8:17){
   
   maintaining[,i+10] <-  
            (maintaining[,i] - min(maintaining[,i], na.rm = TRUE))/ 
@@ -83,7 +108,7 @@ for(i in 2:11){
 
 write.csv(maintaining, "Ranking/AHP/estuary_assessment_maintaining_data.csv")
 
-for(i in 2:11){
+for(i in 8:17){
   
   improving[,i+10] <-  
     (improving[,i] - min(improving[,i], na.rm = TRUE))/ 
@@ -95,7 +120,7 @@ for(i in 2:11){
 
 write.csv(improving, "Ranking/AHP/estuary_assessment_improving_data.csv")
 
-for(i in 2:11){
+for(i in 8:17){
   
   shorebirds[,i+10] <-  
     (shorebirds[,i] - min(shorebirds[,i], na.rm = TRUE))/ 
@@ -107,7 +132,7 @@ for(i in 2:11){
 
 write.csv(shorebirds, "Ranking/AHP/estuary_assessment_shorebird_data.csv")
 
-for(i in 2:11){
+for(i in 8:17){
   
   waterfowl[,i+10] <-  
     (waterfowl[,i] - min(waterfowl[,i], na.rm = TRUE))/ 
